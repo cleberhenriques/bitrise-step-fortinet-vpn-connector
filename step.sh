2d878eebc8f830ee8202284ebb4379b4ed189554
Cleber Henriques <cleber.neto@agiplan.com.br>
2018-09-12 09:43:15 -0300

    Removing unnecessary empty lines

--- step.sh
+++ step.sh
@@ -6,11 +6,9 @@ echo "Update repositories, installing ppp and openfortivpn"
 sudo apt-get update && sudo apt-get install -y ppp && sudo apt-get install -y openfortivpn
 
 echo "Installing iputils"
-
 sudo apt-get install -y iputils-ping
 
 echo "Starting VPN connection with gateway - ${host}:${port}"
-
 sudo nohup openfortivpn ${host}:${port} --password=${password} --username=${username} --trusted-cert ${trusted_cert} &> $BITRISE_DEPLOY_DIR/logs.txt & disown
 
 echo "Waiting connection"
