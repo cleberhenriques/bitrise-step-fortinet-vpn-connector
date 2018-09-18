927eb2f591e0a88133840bb6279d766f363c926b
Cleber Henriques <cleber.neto@agiplan.com.br>
2018-09-18 08:13:23 -0300

    Verifying if OS is Linux or Mac

--- step.sh
+++ step.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-echo "Adding universe repository to apt-get"
-sudo add-apt-repository "deb http://cz.archive.ubuntu.com/ubuntu cosmic main universe"
+set -x
 
-echo "Update repositories, installing ppp and openfortivpn"
-sudo apt-get update && sudo apt-get install -y ppp && sudo apt-get install -y openfortivpn
+
+if [[ "$unamestr" == 'Linux' ]]; then
+
+  echo "Adding universe repository to apt-get"
+  sudo add-apt-repository "deb http://cz.archive.ubuntu.com/ubuntu cosmic main universe"
+
+  echo "Update repositories, installing ppp and openfortivpn"
+  sudo apt-get update && sudo apt-get install -y ppp && sudo apt-get install -y openfortivpn
+
+else
+
+  echo "Installing openfortivpn on MacOS"
+  sudo brew install -y openfortivpn
+
+fi
 
 echo "Starting VPN connection with gateway - ${host}:${port}"
 sudo nohup openfortivpn ${host}:${port} --password=${password} --username=${username} --trusted-cert ${trusted_cert} &> $BITRISE_DEPLOY_DIR/logs.txt & disown
